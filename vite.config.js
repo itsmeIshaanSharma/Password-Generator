66e8b67686cfc6567b4c5c97b93a29530e7b268c
Ishaan Sharma <ishaans502@gmail.com>
2025-02-16 02:26:24 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base : "/Result-Summary-Component/",
+  base : "/ResultSummary-Component/",
   plugins: [
     react(),
     tailwindcss()
